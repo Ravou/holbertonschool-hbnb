9ec6dcd244295402188d80845c3a4bc0ba3edab6
Ravou <oliviaravou@hotmail.fr>
2025-06-18 13:23:43 +0200

    change attribut private to public

--- part2/app/models/base_model.py
+++ part2/app/models/base_model.py
@@ -3,9 +3,9 @@ from datetime import datetime
 
 class BaseModel:
     def __init__(self, id=None, created_at=None, updated_at=None):
-        self.__id = id or str(uuid.uuid4())
-        self.__created_at = created_at or datetime.now()
-        self.__updated_at = updated_at or datetime.now()
+        self.id = id or str(uuid.uuid4())
+        self.created_at = created_at or datetime.now()
+        self.updated_at = updated_at or datetime.now()
 
     def save(self):
         self.updated_at = datetime.now()
