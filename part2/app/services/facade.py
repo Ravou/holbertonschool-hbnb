79c01b807e493dfc9464e487ecd3bd8f1ca2bb7d
Ravou <oliviaravou@hotmail.fr>
2025-06-22 21:45:07 +0200

    add name

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -79,17 +79,25 @@ class HBnBFacade:
     # --------- AMENITY ----------
     def create_amenity(self, amenity_data):
         amenity = Amenity(**amenity_data)
-        self.repo.add(amenity)
+        self.amenity_repo.add(amenity)
         return amenity
 
     def get_amenity(self, amenity_id):
-        return self.repo.get(amenity_id)
+        return self.amenity_repo.get(amenity_id)
+    
+    def get_amenity_by_name(self, amenity_name):
+        return self.amenity_repo.get_by_attribute('name', amenity_name)
 
     def get_all_amenities(self):
-        return self.repo.get_all()
+        return self.amenity_repo.get_all()
 
     def update_amenity(self, amenity_id, amenity_data):
-        self.repo.update(amenity_id, amenity_data)
-        return self.repo.get(amenity_id)
+        amenity = self.amenity_repo.get(amenity_id)
+        if not amenity:
+            return None
+        if 'name' in amenity_data:
+            amenity.name = amenity_data['name']
+            self.amenity_repo.update(amenity_id, amenity_data)
+            return amenity
 
 facade = HBnBFacade()
