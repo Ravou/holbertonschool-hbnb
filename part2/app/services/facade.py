6525f1d2bc5390547bae65b0e59952178ede0a0d
Ravou <oliviaravou@hotmail.fr>
2025-06-22 10:14:35 +0200

    connect facade to repository

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -12,58 +12,60 @@ class HBnBFacade:
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
+        self.reservation_repo = InMemoryRepository()
 
 # --------- USER ----------
     def create_user(self, first_name: str, last_name: str, email: str, is_admin=False) -> User:
         user = User(first_name, last_name, email, is_admin)
-        self.users.append(user)
+        self.users_repo.add(user)
         return user
 
     def get_user(self, user_id: str) -> Optional[User]:
-        return next((u for u in self.users if u.id == user_id), None)
+        return self.user_repo.get(user_id)
 
     def list_users(self) -> List[User]:
-        return self.users
+        return self.user_repo.all()
 
     # --------- PLACE ----------
     def create_place(self, title: str, description: str, price: float, latitude: float, longitude: float, owner: User) -> Place:
         place = Place(title, description, price, latitude, longitude, owner)
-        self.places.append(place)
+        self.places_repo.add(place)
         owner.add_place(place)
         return place
 
     def get_place(self, place_id: str) -> Optional[Place]:
-        return next((p for p in self.places if p.id == place_id), None)
+        return self.place_repo.get(place_id)
 
     def list_places(self) -> List[Place]:
-        return self.places
+        return self.places_repo.all()
 
     # --------- REVIEW ----------
     def create_review(self, user: User, place: Place, content: str, rating: int) -> Review:
-        review = Review(user, place, content, rating)
-        self.reviews.append(review)
+        review = Review.create_review(user, place, content, rating)
+        self.reviews_repo(review)
         user.add_review(review)
         place.add_review(review)
         return review
 
     def list_reviews(self) -> List[Review]:
-        return self.reviews
+        return self.review_repo.all()
+
 
     # --------- RESERVATION ----------
     def create_reservation(self, user: User, place: Place, start_date, end_date) -> Reservation:
         reservation = Reservation(user, place, start_date, end_date)
-        self.reservations.append(reservation)
+        self.reservations_repo.add(reservation)
         user.add_reservation(reservation)
         return reservation
 
     def list_reservations(self) -> List[Reservation]:
-        return self.reservations
+        return self.reservations_repo.all()
 
     # --------- AMENITY ----------
     def create_amenity(self, name: str, description: str) -> Amenity:
         amenity = Amenity(name, description)
-        self.amenities.append(amenity)
+        self.amenities_repo.add(amenity)
         return amenity
 
     def list_amenities(self) -> List[Amenity]:
-        return self.amenities
+        return self.amenities_repo.all()
