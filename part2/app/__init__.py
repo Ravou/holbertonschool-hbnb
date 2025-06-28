73c57a8a08e7fdc143eb6c79cbccdda4d29ec957
Ravou <oliviaravou@hotmail.fr>
2025-06-28 11:50:29 +0200

    fix ad to as

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -4,7 +4,7 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.reservations import api ad reservations_ns
+from app.api.v1.reservations import api as reservations_ns
 
 
 def create_app():
