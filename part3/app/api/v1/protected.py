e5f2499d15959f9ef5c2e05f5c55db97bea07f17
Ravou <oliviaravou@hotmail.fr>
2025-07-05 19:02:59 +0200

    add

--- part3/app/api/v1/protected.py
+++ part3/app/api/v1/protected.py
@@ -1,7 +1,8 @@
 from flask_restx import Namespace, Resource
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import get_jwt
 
-# Namespace sans préfixe interne, car on veut que l’URL finale soit /api/v1/protected
 protected_ns = Namespace(
     'protected',
     description='Zone protégée',
@@ -23,5 +24,8 @@ class ProtectedResource(Resource):
     def get(self):
         """A protected endpoint that requires a valid JWT token"""
         current_user = get_jwt_identity()
-        return {'message': f'Hello, user {current_user["id"]}, admin: {current_user["is_admin"]}'}, 200
+        claims = get_jwt()
+        is_admin = claims.get('is_admin', False)
+
+        return {'message': f'Hello, user {current_user["id"]}, admin: {is_admin}'}, 200
 
